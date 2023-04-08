e455b2ea94dd44aa42949012c76291d3bea8cda7
Federico Sartoris <fsartoris@gmail.com>
2023-04-08 13:05:05 -0300

    tweak libs files

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='football_viz',
+    name='footballviz',
     version='0.1.0',
-    packages=find_packages(),
+    py_modules=['footballviz'],
     install_requires=[
-        'matplotlib',        
+        'matplotlib',
         'adjustText',
     ],
 )
